6ad061e86b76af25347573cdf74b50c989a54f06
jung0730 <arj2763@gmail.com>
2024-01-02 17:26:18 +0800

    maxProfit.js

---
+++ maxProfit.js
@@ -0,0 +1,19 @@
+// tips: sliding window
+
+var maxProfit = function(prices) {
+  let left = 0
+  let right = 1
+  let max = 0
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      const profit = prices[right] - prices[left]
+      max = Math.max(profit, max)
+    } else {
+      left = right
+    }
+    right ++
+  }
+  return max
+};
+
+console.log(maxProfit([7,1,5,3,6,4]))
