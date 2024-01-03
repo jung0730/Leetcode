334da4d6c44c4563660aab3b361638c61b8cc5f3
jung0730 <arj2763@gmail.com>
2024-01-03 17:17:45 +0800

    mergeTwoLists.js

---
+++ mergeTwoLists.js
@@ -0,0 +1,44 @@
+// Definition for singly-linked list
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+// Function to create a linked list from an array of values
+function createLinkedList(values) {
+  if (values.length === 0) {
+      return null;
+  }
+
+  let head = new ListNode(values[0]);
+  let current = head;
+
+  for (let i = 1; i < values.length; i++) {
+      current.next = new ListNode(values[i]);
+      current = current.next;
+  }
+
+  return head;
+}
+
+// tips: dummy node technique
+var mergeTwoLists = function(list1, list2) {
+  let dummyNode = new ListNode()
+  let currentNode = dummyNode
+  while (list1 && list2) { // 當不是null時跳出loop
+    if (list1.val < list2.val) {
+      currentNode.next = list1
+      list1 = list1.next
+    } else {
+      currentNode.next = list2
+      list2 = list2.next
+    }
+    currentNode = currentNode.next
+  }
+  currentNode.next = list1 || list2
+  return dummyNode.next
+};
+
+const linkedList1 = createLinkedList([1,2,4])
+const linkedList2 = createLinkedList([1,3,4])
+console.log(mergeTwoLists(linkedList1, linkedList2))
