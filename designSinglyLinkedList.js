b6c56bec5853be2212304ab4b6e442ac45c07ee1
Hsin Jung <arj2763@gmail.com>
2025-02-03 16:14:18 +0800

    更新singly linked list

--- designSinglyLinkedList.js
+++ designSinglyLinkedList.js
@@ -24,113 +24,97 @@ class SinglyLinkedList {
     this.size ++;
   }
 
-  // ex: this.head -> [10] -> [20] -> [30]
-  // 需求是變成 this.head -> [5] -> [10] -> [20] -> [30]
   insert(index, value) {
     if (index < 0 || index > this.size) {
       return;
     }
     const newNode = new Node(value);
+    // ex: this.head -> [10] -> [20] -> [30]
+    // 需求是變成 this.head -> [5] -> [10] -> [20] -> [30]
     if (index === 0) { // 插入頭部
       newNode.next = this.head;
 
-      // 雖然已經執行了 newNode.next = this.head，讓新節點的 next 指向了 10，但這樣做並不會改變 this.head 本身的指向。
+      // 雖然已經執行了 newNode.next = this.head，讓新節點的 next 指向了 5，但這樣做並不會改變 this.head 本身的指向。
       // 結果是：新節點 5 並沒有被設為鏈結串列的頭部，整個鏈結串列的結構還是以 10 為開頭。
       this.head = newNode;
+    } else {
+      let current = this.head;
+      let previous = null; // 用來記錄current的前一個節點
+      let i = 0;
+      // 找到index 位置
+      while (i < index) {
+        previous = current;
+        current = current.next;
+        i++;
+      }
+      // 找到以後
+      previous.next = newNode;
+      newNode.next = current;
     }
+    this.size++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.size) {
+      return;
+    }
+    let removeNode;
+    if (index === 0) {
+      removeNode = this.head; // 紀錄刪除的值是多少
+      this.head = this.head.next; // 讓頭部變成原本的第二個節點
+      // 這樣原本的第一個節點就沒有人指向它，自動從鏈結串列中移除
+    } else {
+      let current = this.head;
+      let previous = null;
+      let i = 0;
+      while (i < index) {
+        previous = current;
+        current = current.next;
+        i++;
+      }
+      removeNode = current; // 指向要刪除的節點
+      previous.next = current.next; // 讓 previous.next 直接跳過 current，指向 current.next，這樣 current 就被移除。
+    }
+    this.size --;
+    return removeNode.value;
+  }
+
+  display() {
+    let current = this.head;
+    const values = [];
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+    console.log(values.join(" -> "));
+  }
+
+  indexOf(value) {
+    let current = this.head;
+    let index = 0;
+    while (current) { // current 不為 null（即還有節點）時，繼續遍歷
+      if (current.value === value) {
+        return index;
+      }
+      current = current.next;
+      index ++;
+    }
+    return -1; // 找不到
   }
 }
 
-//   // 在指定位置插入節點
-//   insert(index, value) {
-//     if (index < 0 || index > this.size) {
-//       console.error("Invalid index");
-//       return;
-//     }
-//     const newNode = new Node(value);
-//     if (index === 0) {
-//       newNode.next = this.head; // 新節點的 next 指向原本的頭部
-//       this.head = newNode; // 更新頭部為新節點
-//     } else {
-//       let current = this.head;
-//       let previous = null;
-//       let i = 0;
-//       while (i < index) {
-//         previous = current;
-//         current = current.next;
-//         i++;
-//       }
-//       previous.next = newNode; // 前一個節點指向新節點
-//       newNode.next = current;  // 新節點指向當前節點
-//     }
-//     this.size++;
-//   }
-
-//   // 刪除指定位置的節點
-//   remove(index) {
-//     if (index < 0 || index >= this.size) {
-//       console.error("Invalid index");
-//       return;
-//     }
-//     let removedNode;
-//     if (index === 0) {
-//       removedNode = this.head; // 取得要移除的節點
-//       this.head = this.head.next; // 更新頭部為下一個節點
-//     } else {
-//       let current = this.head;
-//       let previous = null;
-//       let i = 0;
-//       while (i < index) {
-//         previous = current;
-//         current = current.next;
-//         i++;
-//       }
-//       removedNode = current;
-//       previous.next = current.next; // 跳過要移除的節點
-//     }
-//     this.size--;
-//     return removedNode.value; // 回傳移除的值
-//   }
-
-//   // 查找指定值的位置
-//   indexOf(value) {
-//     let current = this.head;
-//     let index = 0;
-//     while (current) {
-//       if (current.value === value) {
-//         return index;
-//       }
-//       current = current.next;
-//       index++;
-//     }
-//     return -1; // 找不到回傳 -1
-//   }
-
-//   // 顯示整個串列
-//   display() {
-//     let current = this.head;
-//     const values = [];
-//     while (current) {
-//       values.push(current.value);
-//       current = current.next;
-//     }
-//     console.log(values.join(" -> "));
-//   }
-// }
-
-// // 測試
 const list = new SinglyLinkedList();
 list.append(10);
 list.append(20);
 list.append(30);
-// list.display(); // 10 -> 20 -> 30
+list.display(); // 10 -> 20 -> 30
 
 list.insert(0, 5);
-console.log(list.head)
-// list.display(); // 10 -> 15 -> 20 -> 30
+list.insert(1, 15);
+list.display(); // 5 -> 15 -> 10 -> 20 -> 30
 
-// console.log(list.remove(2)); // 移除值為 20 的節點
-// list.display(); // 10 -> 15 -> 30
+console.log(list.remove(2)); // 移除值為 20 的節點
+list.display(); // 5 -> 15 -> 20 -> 30
 
-// console.log(list.indexOf(15)); // 1
-// console.log(list.indexOf(100)); // -1
+console.log(list.indexOf(15)); // 1
+console.log(list.indexOf(100)); // -1
